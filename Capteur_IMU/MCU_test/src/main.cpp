27e24ea63d2114a83084fbc44c4ebf9ab9001fba
Ding-Jeremy <145600655+Ding-Jeremy@users.noreply.github.com>
2026-02-23 18:32:44 +0100

    First readings with the MPU

--- Capteur_IMU/MCU_test/src/main.cpp
+++ Capteur_IMU/MCU_test/src/main.cpp
@@ -1,15 +1,40 @@
 #include <Arduino.h>
+#include "I2Cdev.h"
+#include "MPU6050.h"
+MPU6050 mpu;
 
-// put function declarations here:
+int16_t ax, ay, az;
+int16_t gx, gy, gz;
 
 void setup()
 {
-  // put your setup code here, to run once:
-  Serial.begin(9600);
-  Serial.println("Hello World !");
+  Serial.begin(115200);
+  Wire.begin();
+
+  Serial.println("Initializing MPU6050...");
+  mpu.initialize();
+
+  if (mpu.testConnection())
+  {
+    Serial.println("MPU6050 connection successful");
+  }
+  else
+  {
+    Serial.println("MPU6050 connection failed");
+    while (1)
+      ;
+  }
 }
 
 void loop()
 {
-  // put your main code here, to run repeatedly:
+  mpu.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
+
+  Serial.print(ax);
+  Serial.print(" ");
+  Serial.print(ay);
+  Serial.print(" ");
+  Serial.println(az);
+
+  delay(20);
 }
